3906c8bf8bf4b8da4a9676238f54e64940ceaf49
Edward Hotchkiss <edward@edwardhotchkiss.com>
2012-03-24 20:28:21 -0400

    [initial]

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# node-angular [![Build Status](https://secure.travis-ci.org/edwardhotchkiss/node-angular.png)](http://travis-ci.org/edwardhotchkiss/node-angular)
+
+> Node.JS + Angular.JS project generator
+
+Install:
+
+```bash
+$ sudo npm install angular -g
+```
+
+Generate New Project:
+
+```bash
+$ angular new blog && cd blog
+$ npm install
+$ angular server 8080
+$ open http://localhost:8080/
+```
+
+## License (MIT)
+
+Copyright (c) 2012, Edward Hotchkiss.
+
+### Author: [Edward Hotchkiss][0]
+
+[0]: http://edwardhotchkiss.com/
---
+++ bin/angular.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+var args = process.argv.slice(2)
+  , command = args.shift()
+  , angular = new require('../lib/node-angular');
+
+require('colors');
+
+switch(command) {
+  case 'v':
+  case 'version':
+  case '-v':
+  case '--version':
+    console.log(angular.version().green);
+    break;
+  case 'n':
+  case 'new':
+    angular.init(args);
+    break;
+  case 'g':
+  case 'generate':
+  case '-g':
+  case '-generate':
+    angular.generate(args);
+    break;
+  case 'r':
+  case 'routes':
+  case '-r':
+  case '--routes':
+    angular.routes(args);
+    break;
+  case 's':
+  case 'server':
+  case '-s':
+  case '--server':
+    angular.server();
+    break;
+  case 'h':
+  case 'help':
+  case '-h':
+  case '--help':
+  default:
+    angular.help();
+    break;
+};
+
+process.exit(0);
+
+/* EOF */
